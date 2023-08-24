2d594de0faff8ba79fbcd0faa3e8fcbea98a9028
Olena <alyonaeremen@gmail.com>
2023-08-24 09:20:52 +0000

    Add more product items in object

--- src/script.js
+++ src/script.js
@@ -12,3 +12,40 @@ if(closeMenu){
     })
 }
 
+//Select container in which I will insert products from items.js
+const productSummerContainer=document.querySelector(".products-container");
+const allProductsContainer=document.querySelector(".all-items");
+
+summerCollection.forEach((product)=>{
+    productSummerContainer.innerHTML+= `
+    <div class="pro">
+          <img
+            src="${product.imgSrc}"
+            alt="air force sport shoes"
+          />
+          <div class="description">
+            <p>${product.brand}</p>
+            <h4>${product.name}</h4>
+            <h3>${product.price}$</h3>
+          </div>
+          <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+        </div>
+    `
+})
+
+allProducts.forEach((product)=>{
+    allProductsContainer.innerHTML+=`
+    <div class="pro" onclick="window.location.href='product.html';">
+          <img
+            src="${product.imgSrc}"
+            alt="air force sport shoes"
+          />
+          <div class="description">
+            <p>${product.brand}</p>
+            <h4>${product.name}</h4>
+            <h3>${product.price}$</h3>
+          </div>
+          <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+        </div>
+    `
+})
